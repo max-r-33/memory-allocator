db1f5e69d3debf08d9ecbe8617cadb0a525c6f6c
max-r-33 <max.rodewald@gmail.com>
2018-12-04 11:28:01 +0100

    initial commit

---
+++ alloc.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+void *mymalloc(size_t size)
+{
+    return NULL;
+}
+
+void *mycalloc(size_t nmemb, size_t size)
+{
+    return NULL;
+}
+
+void myfree(void *ptr)
+{
+}
+
+void *myrealloc(void *ptr, size_t size)
+{
+    return NULL;
+}
+
+
+/*
+ * Enable the code below to enable system allocator support for your allocator.
+ */
+#if 0
+void *malloc(size_t size) { return mymalloc(size); }
+void *calloc(size_t nmemb, size_t size) { return mycalloc(nmemb, size); }
+void *realloc(void *ptr, size_t size) { return myrealloc(ptr, size); }
+void free(void *ptr) { myfree(ptr); }
+#endif
